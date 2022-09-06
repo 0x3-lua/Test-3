fd40707a8a0f327fce7acb293beaa2db74df09ce
CHL-a <eacclickbait@gmail.com>
2022-09-06 09:44:25 -0700

    1.72

--- src_V1/main.lua
+++ src_V1/main.lua
@@ -1,12 +1,19 @@
 --run code below
 local Static = require('Static')
 -- local WebServerV3 = require('WebServerV3')
-Static.luarocks.loadModule('LuaSocket')
-Static.luarocks.loadModule('luasec')
-Static.luarocks.loadModule('lsocket')
+Static.luarocks.loadModule('pegasus')
+local pegasus = require 'pegasus'
 
-print(Static.table.toString(require('ssl')))
+local server = pegasus:new({
+  port='8080',
+  location='example/root'
+})
 
+server:start(function (request, response)
+	print "It's running..."
+  
+  print(Static.table.toString(request), Static.table.toString(response))
+end)
 
 --[[
 local WebServer = require('WebServer')
