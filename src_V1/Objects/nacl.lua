e1c622056615088d4478663c511c80bd292af34c
CHL-a <eacclickbait@gmail.com>
2022-09-18 09:02:29 -0700

    2.49

--- src_V1/Objects/nacl.lua
+++ src_V1/Objects/nacl.lua
@@ -731,8 +731,9 @@ function crypto_hashblocks(result, array, n)
 	  
 		pos = pos + 128;
 		n = n - 128;
-	end
-	print(Static.table.toString(z),Static.table.toString(b),Static.table.toString(a),Static.table.toString(w))
+    end
+	-- check b
+	print(Static.table.toString(t),Static.table.toString(array))
 
 	for i = 1, 8 do ts64(result, 8*(i - 1), z[i]) end
 	return n
