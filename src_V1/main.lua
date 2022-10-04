6c1846f2fa2a734a8bdaa046f914542726ea989f
CHL-a <eacclickbait@gmail.com>
2022-10-04 09:12:41 -0700

    5.93

--- src_V1/main.lua
+++ src_V1/main.lua
@@ -3,9 +3,19 @@ local Static = require('Static')
 local DiscordBot = require('DiscordBot')
 local Bot = DiscordBot.new() -- argument optional if environment variable `DiscordBotToken` is correctly set
 
-.setRequestCallback(function (c, req, res)
+	.setRequestCallback(function(c, req, res)
+	res.headers.connection = 'close'
+	if req.webPage == '/' then
+		res.body = 'home'
+		res.statusCode = 200;
+		res.statusMessage = 'ok'
+		return;
+	end
+
+	
+
 	print('met', Static.table.toString(req))
 end)
 
 
-.run() -- last step
+	.run() -- last step
