11a1242f314614aa4bb2d2fdc8258e355204bf3c
CHL-a <eacclickbait@gmail.com>
2022-10-06 15:52:29 -0700

    5.117

--- src_V1/Objects/Luvit.lua
+++ src_V1/Objects/Luvit.lua
@@ -12,7 +12,7 @@ local Static = require'Static'
 local current = Static.os.runBash('pwd')
 Static.os.runBash('cd ~/')
 
-local folderExists = Static.os.runBash('if [-e luvit]; then\n\techo 1;\nelse\n\techo 0;\nfi') == '1'
+local folderExists = Static.os.runBash('test -d luvit && echo 1 || echo 0') == '1'
 
 print(folderExists)
 
