3d57e6093f795ce9dc768b95d87b02875e2d51cb
CHL <eaclickbait@gmail.com>
2022-11-01 12:39:25 -0700

    5.169

--- init.lua
+++ init.lua
@@ -1,27 +1,6 @@
 -- load path
-package.path = package.path .. ';./src_V1/?.lua;./src_V1/Objects/?.lua;./src_V1/Objects/?/init.lua'
 
 -- load luvit and discordia
-
-local Luvit = require('Luvit').install('SinisterRectus/discordia')
-local Static = require('Static')
-
-do
-	print(Static.table.toString(Luvit))
-
-	 return
-end
-
-local path = Static.os.runBash('pwd'):sub(1, -2)
-
-local command = ('RelativePath="%s";cd %s;./luvit %s/init.lua'):format(
-	path,
-	Luvit.path,
-	path
-)
-
-io.write(Static.os.runBash(command)) -- possible debugging
-
 
 
 
