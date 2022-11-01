d573d93e49d5a6ac75fe3dd72912a8ab926c3b97
CHL <eaclickbait@gmail.com>
2022-11-01 13:10:34 -0700

    5.172

--- init.lua
+++
@@ -1,4 +0,0 @@
--- load path
-package.path = package.path .. ';./src_V1/?.lua;./src_V1/Objects/?.lua;./src_V1/Objects/?/init.lua'
-
-return true;
--- main.lua
+++ main.lua
@@ -1,11 +1,10 @@
 -- [pre]
-assert(require('init'), 'loading initial failed, check ')
+-- assert(require('init'), 'loading initial failed, check file')
+package.path = package.path .. ';./src_V1/?.lua;./src_V1/Objects/?.lua;./src_V1/Objects/?/init.lua'
 
 -- [main]
 local Static = require('Static')
 local discordia = require('discordia')
 
-print(Static.table.toString(discordia))
-
--- post
--- return true
+-- print(Static.table.toString(discordia))
+print('done')
